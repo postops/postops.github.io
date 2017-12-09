868e33329f3e6a163a8a3dce434587a2eb42478c
JWal31 <Jay.Wallace@nike.com>
2017-12-08 19:29:32 -0800

    fix push script

--- scripts/push_cache.sh
+++ scripts/push_cache.sh
@@ -6,6 +6,6 @@ echo "Pushing up crosspost cache"
 git config user.name "$CIRCLE_USERNAME"
 git config user.email "$USER_EMAIL"
 
-git add -fA
+git add .
 git commit --allow-empty -m "$(git log -1 --pretty=%B) - crosspost cache [ci skip]"
 git push -f $CIRCLE_REPOSITORY_URL master
